4c3e8420c68256e8f406d55b248889da372570a8
Prateek <prateekchandra9687@gmail.com>
2023-06-25 10:26:44 +0530

    #3

--- script.js
+++ script.js
@@ -29,6 +29,12 @@ window.onscroll = () => {
                 links.classList.remove('active')
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            //active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeates on scroll use this
+        else{
+            sec.classList.remove('show-animate');
         }
     });
 }
@@ -43,6 +49,12 @@ window.onscroll = () => {
     // remove toggle icon and navbar when click navbar links (scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+
+    // animation on footer on scroll
+
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
 
 
